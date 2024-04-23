9e0b142bfd86c93879886f418de7c24dcaada693
soha100 <sohaahmed718@gmail.com>
2024-04-23 17:21:40 +0200

    slideShow

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// slidShow
+
+let landing = document.querySelector(".landing");
+
+let arr = [
+  "url('../imgs/landing-1.jpg')",
+  "url('../imgs/landing-2.jpg')",
+  "url('../imgs/landing-3.jpg')",
+  "url('../imgs/landing-4.jpg')",
+];
+let i = 0;
+
+function slideShow() {
+  landing.style.backgroundImage = arr[i];
+  if (i == arr.length - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  setTimeout(() => slideShow(), 3000);
+}
+
+slideShow();
