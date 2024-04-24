ac871a46f957ff6530c47a580c808fae912145b9
Safeya-Yasien <bluesky7yas@gmail.com>
2024-04-24 02:05:32 +0200

    #fix fonts & fixed header

--- js/index.js
+++ js/index.js
@@ -3,21 +3,28 @@
 let landing = document.querySelector(".landing");
 
 let arr = [
-  "url('imgs/landing-1.jpg')",
-  "url('imgs/landing-2.jpg')",
-  "url('imgs/landing-3.jpg')",
-  "url('imgs/landing-4.jpg')",
+  "imgs/landing-1.jpg",
+  "imgs/landing-3.jpg",
+  "imgs/landing-4.jpg",
+  "imgs/landing-5.jpg",
+  "imgs/landing-6.png",
+  "imgs/landing-2.jpg",
+  "imgs/landing-7.png",
+  "imgs/landing-8.jpg",
+  "imgs/landing-10.png",
+  "imgs/landing-11.png",
 ];
+
 let i = 0;
 
 function slideShow() {
-  landing.style.backgroundImage = arr[i];
+  landing.style.backgroundImage = `url(${arr[i]})`;
   if (i == arr.length - 1) {
     i = 0;
   } else {
     i++;
   }
-  setTimeout(() => slideShow(), 3000);
+  setTimeout(slideShow, 3000);
 }
 
 slideShow();
